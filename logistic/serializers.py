26351a93c996178b10228562b089d418c5d7ddb4
Timur Kotiev <144617836+CatGaster@users.noreply.github.com>
2024-04-09 17:32:58 +0300

    delete serializers comments

--- logistic/serializers.py
+++ logistic/serializers.py
@@ -23,27 +23,15 @@ class StockSerializer(serializers.ModelSerializer):
         fields = ['id', 'address', 'positions']
 
     def create(self, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-        
-
-        # создаем склад по его параметрам
         stock = super().create(validated_data)
 
-
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
         for position in positions:
             StockProduct.objects.create(stock=stock, product = position['product'], quantity=position['quantity'], price=position['price'])
-
         return stock
 
     def update(self, instance, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
-        # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
 
         for position in positions:
