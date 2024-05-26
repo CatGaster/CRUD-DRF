cd4cb0abf27f1d097b9f8dd93000052f2fa55c77
Kotiev Timur <nick.hoult@mail.ru>
2024-05-26 14:22:59 +0300

    tests5

--- logistic/views.py
+++ logistic/views.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 
 from logistic.models import Product, Stock
@@ -21,3 +23,8 @@ class StockViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["products"] 
     search_fields = ['products__title', 'products__description',]
+
+
+@api_view(['GET'])
+def sample_view(request):
+    return Response({'message': 'Всё работает!!! УРАА!!!!'})
